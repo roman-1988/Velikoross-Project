835a1537da1af0c36dc3b11a8e231f8141ee2d35
Roman <roman-1@inbox.ru>
2023-08-02 21:59:09 +0300

    Создал проект на Vite. Подключил библиотеку MUI. Создал header.

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { AppBar } from "@mui/material"
+import "./App.css"
+
+const App = () => {
+
+  return (
+    <>
+      <AppBar>
+        <h1>Великая Россия</h1>
+      </AppBar>
+    </>
+  )
+}
+
+export { App } 
