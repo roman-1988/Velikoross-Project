bbad145e3f675f1c65c74b43548a3a6e57be59e7
Roman <roman-1@inbox.ru>
2023-11-13 22:56:02 +0300

    Верстка Menu - раздел книги

--- Frontend/src/components/Menu/Menu.tsx
+++ Frontend/src/components/Menu/Menu.tsx
@@ -1,4 +1,6 @@
-import { Box, Drawer, IconButton, List, ListItem, ListItemText, ListItemButton, ListItemIcon, ListItemAvatar, Avatar, Divider } from "@mui/material"
+import { useState } from "react"
+import { Box, Drawer, IconButton, List, ListItemText, ListItemButton, ListItemIcon, Collapse, Divider }
+    from "@mui/material"
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft"
 import PublicIcon from "@mui/icons-material/Public"
 import FeedIcon from "@mui/icons-material/Feed"
@@ -6,13 +8,21 @@ import HistoryEduIcon from "@mui/icons-material/HistoryEdu"
 import AutoStoriesIcon from "@mui/icons-material/AutoStories"
 import ForumIcon from "@mui/icons-material/Forum"
 import MarkAsUnreadIcon from "@mui/icons-material/MarkAsUnread"
+import ExpandLess from "@mui/icons-material/ExpandLess"
+import ExpandMore from "@mui/icons-material/ExpandMore"
+import CreateIcon from "@mui/icons-material/Create"
 
 const Menu = (props) => {
+    const [open, setOpen] = useState(false)
 
     const {
         menuOpen,
         menuClose = Function.prototype,
     } = props
+
+    const handleClick = () => {
+        setOpen(!open)
+    }
 
     return (
         <Drawer
@@ -53,12 +63,53 @@ const Menu = (props) => {
                         <ListItemText primary="Документы" />
                     </ListItemButton>
                     <Divider variant="inset" component="li" />
-                    <ListItemButton>
+                    <ListItemButton onClick={handleClick}>
                         <ListItemIcon>
                             <AutoStoriesIcon />
                         </ListItemIcon>
                         <ListItemText primary="Книги" />
+                        {open ? <ExpandLess /> : <ExpandMore />}
                     </ListItemButton>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <List component="div" disablePadding>
+                            <ListItemButton sx={{ pl: 4 }}>
+                                <ListItemIcon>
+                                    <CreateIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Андрей Савельев" />
+                            </ListItemButton>
+                            <ListItemButton sx={{ pl: 4 }}>
+                                <ListItemIcon>
+                                    <CreateIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Иван Ильин" />
+                            </ListItemButton>
+                            <ListItemButton sx={{ pl: 4 }}>
+                                <ListItemIcon>
+                                    <CreateIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Константин Победоносцев" />
+                            </ListItemButton>
+                            <ListItemButton sx={{ pl: 4 }}>
+                                <ListItemIcon>
+                                    <CreateIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Иван Солоневич" />
+                            </ListItemButton>
+                            <ListItemButton sx={{ pl: 4 }}>
+                                <ListItemIcon>
+                                    <CreateIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Михаил Меньшиков" />
+                            </ListItemButton>
+                            <ListItemButton sx={{ pl: 4 }}>
+                                <ListItemIcon>
+                                    <CreateIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Лев Тихомиров" />
+                            </ListItemButton>
+                        </List>
+                    </Collapse>
                     <Divider variant="inset" component="li" />
                     <ListItemButton>
                         <ListItemIcon>
