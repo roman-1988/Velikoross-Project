f823cf6542ad0160b4a7b6d394ff0baaf78a0833
Roman <roman-1@inbox.ru>
2023-11-16 21:46:54 +0300

    Добавил react-router-dom в зависимости и иконки в Footer

--- Frontend/src/components/Footer/Footer.tsx
+++ Frontend/src/components/Footer/Footer.tsx
@@ -1,19 +1,33 @@
-import { Box, AppBar, Typography } from "@mui/material"
+import { Box, AppBar, Typography, IconButton, Stack } from "@mui/material"
+import FacebookIcon from "@mui/icons-material/Facebook"
 import "./Footer.css"
 
 const Footer = () => {
     return (
         <>
             <Box sx={{
-                flexGrow: 1,
+                flexGrow: 1
             }}>
                 <AppBar sx={{ height: "65.875px" }} position="static">
-                    <Typography>
+                    <Typography component="div" sx={{ flexGrow: 1 }}
+                        ml={1} mt={1}>
                         Copyright © 2023 Великая Россия
                     </Typography>
-                    <Typography>
+                    <Typography component="div" sx={{ flexGrow: 1 }}
+                        ml={1} mb={1}>
                         Карта сайта
                     </Typography>
+                    <Stack direction="row" spacing={1}>
+                    <IconButton>
+                        <FacebookIcon />
+                    </IconButton>
+                    <IconButton>
+                        <FacebookIcon />
+                    </IconButton>
+                    <IconButton>
+                        <FacebookIcon />
+                    </IconButton>
+                    </Stack>
                 </AppBar>
             </Box>
         </>
@@ -21,3 +35,6 @@ const Footer = () => {
 }
 
 export { Footer }
+
+
+
